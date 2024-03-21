5627820929300a1961137ffa7c8ea09c3f361bcf
malix254 <griffinskirimimbae@mail.com>
2024-03-21 13:17:29 +0300

    update1

---
+++ ibrahim.js
@@ -0,0 +1,26 @@
+/ 1.0 ARROW FUNCTION
+//TODO: create a basic function that adds two random values
+
+const addFixedValuesArrow = (x, y) =>  x + y;{
+    
+   
+const result = addvalues(3,5);
+
+    //print our results
+ printValue(sum);
+}
+
+
+//TODO: create a function that takes in two values and adds them
+
+const addFixedValuesArrow => function (){
+    const toyCar = 10;
+    const toyBlock = 7;
+    const sum = toyCar + toyBlock;
+
+    //print our results
+ printValue(sum);
+
+
+};
+//TODO: create a function that takes in two values, adds them and returns the value of the addition
